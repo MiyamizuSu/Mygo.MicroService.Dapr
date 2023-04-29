cd047232c1135cc7e75f1e6ff9e00356d713860d
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-04-29 21:51:12 +0800

    NA

--- Contrib/TextList.Api/Program.cs
+++ Contrib/TextList.Api/Program.cs
@@ -4,9 +4,12 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.AddCustomConfiguration();
 // Console.WriteLine(builder.Configuration["ConnectionStrings:TextListContext"]);
+builder.AddCustomDatabase();
 
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
+
+app.ApplyDatabaseMigration();
 
 app.Run();
--- Contrib/TextList.Api/ProgramExtensions.cs
+++ Contrib/TextList.Api/ProgramExtensions.cs
@@ -1,5 +1,8 @@
 ﻿using Dapr.Client;
 using Dapr.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore;
+using Polly;
+using RecAll.Contrib.TextList.Api.Services;
 
 namespace RecAll.Contrib.TextList.Api;
 
@@ -10,5 +13,33 @@ public static class ProgramExtensions {
         this WebApplicationBuilder builder) {
         builder.Configuration.AddDaprSecretStore("recall-secretstore",
             new DaprClientBuilder().Build());
+    }
+
+    public static void AddCustomDatabase(this WebApplicationBuilder builder) {
+        builder.Services.AddDbContext<TextListContext>(options =>
+            options.UseSqlServer(builder.Configuration["ConnectionStrings:TextListContext"]));
+    }
+
+    public static void ApplyDatabaseMigration(this WebApplication app) {
+        // Apply database migration automatically. Note that this approach is not
+        // recommended for production scenarios. Consider generating SQL scripts from
+        // migrations instead.
+        using var scope = app.Services.CreateScope();
+
+        var retryPolicy = CreateRetryPolicy();
+        var context =
+            scope.ServiceProvider.GetRequiredService<TextListContext>();
+
+        retryPolicy.Execute(context.Database.Migrate);
+    }
+
+    private static Policy CreateRetryPolicy() {
+        return Policy.Handle<Exception>().WaitAndRetryForever(
+            sleepDurationProvider: _ => TimeSpan.FromSeconds(5),
+            onRetry: (exception, retry, _) => {
+                Console.WriteLine(
+                    "Exception {0} with message {1} detected during database migration (retry attempt {2})",
+                    exception.GetType().Name, exception.Message, retry);
+            });
     }
 }
