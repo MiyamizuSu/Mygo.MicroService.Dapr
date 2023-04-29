8423e5b79673cbbc1bfca97e1b2ffa7e263c8171
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-04-29 21:30:09 +0800

    NA

--- Contrib/TextList.Api/Program.cs
+++ Contrib/TextList.Api/Program.cs
@@ -1,4 +1,10 @@
+using RecAll.Contrib.TextList.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.AddCustomConfiguration();
+// Console.WriteLine(builder.Configuration["ConnectionStrings:TextListContext"]);
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
---
+++ Contrib/TextList.Api/ProgramExtensions.cs
@@ -0,0 +1,14 @@
+﻿using Dapr.Client;
+using Dapr.Extensions.Configuration;
+
+namespace RecAll.Contrib.TextList.Api;
+
+public static class ProgramExtensions {
+    public static readonly string AppName = typeof(ProgramExtensions).Namespace;
+
+    public static void AddCustomConfiguration(
+        this WebApplicationBuilder builder) {
+        builder.Configuration.AddDaprSecretStore("recall-secretstore",
+            new DaprClientBuilder().Build());
+    }
+}
