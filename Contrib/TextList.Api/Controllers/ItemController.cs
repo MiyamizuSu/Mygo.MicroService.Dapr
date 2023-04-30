8c0dc7eb1eac74c7159fe248c7114c9e5dbdfba7
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-04-30 16:26:46 +0800

    NA

--- Contrib/TextList.Api/Controllers/ItemController.cs
+++ Contrib/TextList.Api/Controllers/ItemController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RecAll.Contrib.TextList.Api.Commands;
 using RecAll.Contrib.TextList.Api.Models;
 using RecAll.Contrib.TextList.Api.Services;
@@ -18,7 +19,7 @@ public class ItemController {
         _identityService = identityService ??
             throw new ArgumentNullException(nameof(identityService));
     }
-    
+
     [Route("create")]
     [HttpPost]
     // ServiceResultViewModel<string>
@@ -36,5 +37,125 @@ public class ItemController {
         //     .CreateSucceededResult(textItemEntity.Entity.Id.ToString())
         //     .ToServiceResultViewModel();
         return textItemEntity.Entity.Id.ToString();
+    }
+
+    [Route("update")]
+    [HttpPost]
+    public async Task<ActionResult> UpdateAsync(
+        [FromBody] UpdateTextItemCommand command) {
+        var userIdentityGuid = _identityService.GetUserIdentityGuid();
+
+        var textItem = await _textListContext.TextItems.FirstOrDefaultAsync(p =>
+            p.Id == command.Id && p.UserIdentityGuid == userIdentityGuid &&
+            !p.IsDeleted);
+
+        if (textItem is null) {
+            // return ServiceResult
+            //     .CreateFailedResult($"Unknown TextItem id: {command.Id}")
+            //     .ToServiceResultViewModel();
+            return new BadRequestResult();
+        }
+
+        textItem.Content = command.Content;
+        await _textListContext.SaveChangesAsync();
+
+        // return ServiceResult.CreateSucceededResult().ToServiceResultViewModel();
+        return new OkResult();
+    }
+
+    [Route("get/{id}")]
+    [HttpGet]
+    // ServiceResultViewModel<TextItemViewModel>
+    public async Task<ActionResult<TextItem>> GetAsync(int id) {
+        var userIdentityGuid = _identityService.GetUserIdentityGuid();
+
+        var textItem = await _textListContext.TextItems.FirstOrDefaultAsync(p =>
+            p.Id == id && p.UserIdentityGuid == userIdentityGuid &&
+            !p.IsDeleted);
+
+        if (textItem is null) {
+            // return ServiceResult<TextItemViewModel>
+            //     .CreateFailedResult($"Unknown TextItem id: {id}")
+            //     .ToServiceResultViewModel();
+            return new BadRequestResult();
+        }
+
+        return textItem is null
+            // ? ServiceResult<TextItemViewModel>
+            //     .CreateFailedResult($"Unknown TextItem id: {id}")
+            //     .ToServiceResultViewModel()
+            ? new BadRequestResult()
+            // : ServiceResult<TextItemViewModel>
+            //     .CreateSucceededResult(new TextItemViewModel {
+            //         Id = textItem.Id,
+            //         ItemId = textItem.ItemId,
+            //         Content = textItem.Content
+            //     }).ToServiceResultViewModel();
+            : textItem;
+    }
+
+    [Route("getByItemId/{itemId}")]
+    [HttpGet]
+    // ServiceResultViewModel<TextItemViewModel>
+    public async Task<ActionResult<TextItem>> GetByItemId(int itemId) {
+        var userIdentityGuid = _identityService.GetUserIdentityGuid();
+
+        var textItem = await _textListContext.TextItems.FirstOrDefaultAsync(p =>
+            p.ItemId == itemId && p.UserIdentityGuid == userIdentityGuid &&
+            !p.IsDeleted);
+
+        if (textItem is null) {
+            // return ServiceResult<TextItemViewModel>
+            //     .CreateFailedResult($"Unknown TextItem with ItemID: {itemId}")
+            //     .ToServiceResultViewModel();
+            return new BadRequestResult();
+        }
+
+        return textItem is null
+            // ? ServiceResult<TextItemViewModel>
+            //     .CreateFailedResult($"Unknown TextItem with ItemID: {itemId}")
+            //     .ToServiceResultViewModel()
+            ? new BadRequestResult()
+            // : ServiceResult<TextItemViewModel>
+            //     .CreateSucceededResult(new TextItemViewModel {
+            //         Id = textItem.Id,
+            //         ItemId = textItem.ItemId,
+            //         Content = textItem.Content
+            //     }).ToServiceResultViewModel();
+            : textItem;
+    }
+
+    [Route("getItems")]
+    [HttpPost]
+    // ServiceResultViewModel<IEnumerable<TextItemViewModel>>
+    public async Task<ActionResult<IEnumerable<TextItem>>> GetItemsAsync(
+        GetItemsCommand command) {
+        var itemIds = command.Ids.ToList();
+        var userIdentityGuid = _identityService.GetUserIdentityGuid();
+
+        var textItems = await _textListContext.TextItems.Where(p =>
+                p.ItemId.HasValue && itemIds.Contains(p.ItemId.Value) &&
+                p.UserIdentityGuid == userIdentityGuid && !p.IsDeleted)
+            .ToListAsync();
+
+        if (textItems.Count != itemIds.Count) {
+            var missingIds = string.Join(",",
+                itemIds.Except(textItems.Select(p => p.ItemId.Value))
+                    .Select(p => p.ToString()));
+
+            // return ServiceResult<IEnumerable<TextItemViewModel>>
+            //     .CreateFailedResult($"Unknown Item id: {missingIds}")
+            //     .ToServiceResultViewModel();
+            return new BadRequestResult();
+        }
+
+        textItems.Sort((x, y) =>
+            itemIds.IndexOf(x.ItemId.Value) - itemIds.IndexOf(y.ItemId.Value));
+
+        // return ServiceResult<IEnumerable<TextItemViewModel>>
+        //     .CreateSucceededResult(textItems.Select(p => new TextItemViewModel {
+        //         Id = p.Id, ItemId = p.ItemId, Content = p.Content
+        //     })).ToServiceResultViewModel();
+        return textItems;
     }
 }
