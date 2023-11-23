6de58d6d1aa0b7c87117d9b0ecb1fef2ca00ab95
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-11-23 17:26:51 +0800

    NA

---
+++ Infrastructure/Ddd.Infrastructure/MediatorExtension.cs
@@ -0,0 +1,18 @@
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using RecAll.Infrastructure.Ddd.Domain.SeedWork;
+
+namespace Ddd.Infrastructure;
+
+public static class MediatorExtension {
+    public static async Task DispatchDomainEventsAsync(this IMediator mediator, DbContext context) {
+        var domainEntities = context.ChangeTracker.Entries<Entity>().Where(p =>
+            p.Entity.DomainEvents != null && p.Entity.DomainEvents.Any());
+        var domainEvents = domainEntities.SelectMany(p => p.Entity.DomainEvents).ToList();
+
+        domainEntities.ToList().ForEach(domainEntity => domainEntity.Entity.ClearDomainEvents());
+        foreach (var domainEvent in domainEvents) {
+            await mediator.Publish(domainEvent);
+        }
+    }
+}
