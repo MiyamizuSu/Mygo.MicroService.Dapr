388e8f0d4c588a7cf7129cab3517998701242ede
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-11-24 21:38:05 +0800

    NA

--- Core/List/List.Api/Infrastructure/AutofacModules/ApplicationModule.cs
+++ Core/List/List.Api/Infrastructure/AutofacModules/ApplicationModule.cs
@@ -1,4 +1,5 @@
 using Autofac;
+using RecAll.Core.List.Api.Application.Queries;
 using RecAll.Core.List.Domain.AggregateModels.ListAggregate;
 using RecAll.Core.List.Infrastructure.Repositories;
 
@@ -6,6 +7,10 @@ namespace RecAll.Core.List.Api.Infrastructure.AutofacModules;
 
 public class ApplicationModule : Module {
     protected override void Load(ContainerBuilder builder) {
-        builder.RegisterType<ListRepository>().As<IListRepository>().InstancePerLifetimeScope();
+        builder.RegisterType<ListQueryService>().As<IListQueryService>()
+            .InstancePerLifetimeScope();
+
+        builder.RegisterType<ListRepository>().As<IListRepository>()
+            .InstancePerLifetimeScope();
     }
 }
