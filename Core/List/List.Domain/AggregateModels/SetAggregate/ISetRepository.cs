f8d780934e81961bd14824fe4528f6cb78c56e2b
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-11-24 21:52:35 +0800

    NA

--- Core/List/List.Domain/AggregateModels/SetAggregate/ISetRepository.cs
+++
@@ -1,9 +0,0 @@
-using RecAll.Infrastructure.Ddd.Domain.SeedWork;
-
-namespace RecAll.Core.List.Domain.AggregateModels.SetAggregate;
-
-public interface ISetRepository : IRepository<Set> {
-    Set Add(Set set);
-
-    Task<Set> GetAsync(int setId, string userIdentityGuid);
-}
--- Core/List/List.Infrastructure/ListContext.cs
+++ Core/List/List.Infrastructure/ListContext.cs
@@ -6,8 +6,6 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.EntityFrameworkCore.Storage;
 using RecAll.Core.List.Domain.AggregateModels;
-using RecAll.Core.List.Domain.AggregateModels.ItemAggregate;
-using RecAll.Core.List.Domain.AggregateModels.SetAggregate;
 using RecAll.Core.List.Infrastructure.EntityConfigurations;
 using RecAll.Infrastructure.Ddd.Domain.SeedWork;
 
@@ -17,10 +15,6 @@ public class ListContext : DbContext, IUnitOfWork {
     public const string DefaultSchema = "list";
 
     public DbSet<Domain.AggregateModels.ListAggregate.List> Lists { get; set; }
-
-    public DbSet<Set> Sets { get; set; }
-
-    public DbSet<Item> Items { get; set; }
 
     public DbSet<ListType> ListTypes { get; set; }
 
@@ -43,8 +37,6 @@ public class ListContext : DbContext, IUnitOfWork {
     protected override void OnModelCreating(ModelBuilder modelBuilder) {
         modelBuilder.ApplyConfiguration(new ListTypeConfiguration());
         modelBuilder.ApplyConfiguration(new ListConfiguration());
-        modelBuilder.ApplyConfiguration(new SetConfiguration());
-        modelBuilder.ApplyConfiguration(new ItemConfiguration());
     }
 
     public async Task<bool> SaveEntitiesAsync(CancellationToken cancellationToken = default) {
