08e0a42bfaf91164b55ddd83bf8389b2b7a2a808
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-11-24 18:46:52 +0800

    NA

---
+++ Core/List/List.Api/Application/Commands/CreateListCommand.cs
@@ -0,0 +1,16 @@
+using MediatR;
+using RecAll.Infrastructure;
+using TheSalLab.GeneralReturnValues;
+
+namespace RecAll.Core.List.Api.Application.Commands;
+
+public class CreateListCommand : IRequest<ServiceResult> {
+    public string Name { get; set; }
+
+    public int TypeId { get; set; }
+
+    public CreateListCommand(string name, int typeId) {
+        Name = name;
+        TypeId = typeId;
+    }
+}
---
+++ Core/List/List.Api/Application/Commands/CreateListCommandHandler.cs
@@ -0,0 +1,25 @@
+using MediatR;
+using RecAll.Core.List.Api.Infrastructure.Services;
+using RecAll.Core.List.Domain.AggregateModels.ListAggregate;
+using RecAll.Infrastructure;
+using TheSalLab.GeneralReturnValues;
+
+namespace RecAll.Core.List.Api.Application.Commands;
+
+public class CreateListCommandHandler : IRequestHandler<CreateListCommand, ServiceResult> {
+    private readonly IIdentityService _identityService;
+    private readonly IListRepository _listRepository;
+
+    public async Task<ServiceResult> Handle(CreateListCommand command, CancellationToken cancellationToken) {
+        _listRepository.Add(new Domain.AggregateModels.ListAggregate.List(command.Name, command.TypeId,
+            _identityService.GetUserIdentityGuid()));
+        return await _listRepository.UnitOfWork.SaveEntitiesAsync(cancellationToken)
+            ? ServiceResult.CreateSucceededResult()
+            : ServiceResult.CreateFailedResult();
+    }
+
+    public CreateListCommandHandler(IIdentityService identityService, IListRepository listRepository) {
+        _identityService = identityService ?? throw new ArgumentNullException(nameof(identityService));
+        _listRepository = listRepository ?? throw new ArgumentNullException(nameof(listRepository));
+    }
+}
