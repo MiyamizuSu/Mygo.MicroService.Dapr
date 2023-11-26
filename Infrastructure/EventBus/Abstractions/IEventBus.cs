2b4332d8c97fcd5b25e486f97967c9591916c95c
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-11-26 09:31:37 +0800

    NA

--- Infrastructure/EventBus/Abstractions/IEventBus.cs
+++ Infrastructure/EventBus/Abstractions/IEventBus.cs
@@ -4,14 +4,4 @@ namespace RecAll.Infrastructure.EventBus.Abstractions;
 
 public interface IEventBus {
     void Publish(IntegrationEvent @event);
-
-    void Subscribe<TIntegrationEvent, TIntegrationEventHandler>()
-        where TIntegrationEvent : IntegrationEvent
-        where TIntegrationEventHandler :
-        IIntegrationEventHandler<TIntegrationEvent>;
-
-    void Unsubscribe<TIntegrationEvent, TIntegrationEventHandler>()
-        where TIntegrationEvent : IntegrationEvent
-        where TIntegrationEventHandler :
-        IIntegrationEventHandler<TIntegrationEvent>;
 }
