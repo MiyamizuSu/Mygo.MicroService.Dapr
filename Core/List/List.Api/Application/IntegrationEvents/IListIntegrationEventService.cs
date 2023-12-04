effbfd83c4a789386aefba082468f6b903aa025c
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-12-04 21:11:33 +0800

    NA

--- Core/List/List.Api/Application/IntegrationEvents/IListIntegrationEventService.cs
+++
@@ -1,9 +0,0 @@
-using RecAll.Infrastructure.EventBus.Events;
-
-namespace RecAll.Core.List.Api.Application.IntegrationEvents;
-
-public interface IListIntegrationEventService {
-    Task AddAndSaveEventAsync(IntegrationEvent integrationEvent);
-
-    Task PublishEventsAsync(Guid transactionId);
-}
