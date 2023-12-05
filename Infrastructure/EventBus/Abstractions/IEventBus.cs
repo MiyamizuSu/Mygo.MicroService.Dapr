57f77ef1a2aa97bda1c3ddb9d62679052b04f0a3
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-12-05 23:03:11 +0800

    NA

--- Infrastructure/EventBus/Abstractions/IEventBus.cs
+++
@@ -1,7 +0,0 @@
-using RecAll.Infrastructure.EventBus.Events;
-
-namespace RecAll.Infrastructure.EventBus.Abstractions;
-
-public interface IEventBus {
-    Task PublishAsync(IntegrationEvent @event);
-}
--- Infrastructure/IntegrationEventLog/Models/EventState.cs
+++
@@ -1,8 +0,0 @@
-namespace RecAll.Infrastructure.IntegrationEventLog.Models; 
-
-public enum EventState {
-    NotPublished = 0,
-    InProcess = 1,
-    Published = 2,
-    PublishedFailed = 3
-}
