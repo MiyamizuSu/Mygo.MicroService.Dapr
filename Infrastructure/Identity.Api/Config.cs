68bc2e633728a7f6c9e099549c0d6242c150d265
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-12-09 21:15:25 +0800

    NA

---
+++ Infrastructure/Identity.Api/Config.cs
@@ -0,0 +1,12 @@
+using Duende.IdentityServer.Models;
+
+namespace RecAll.Infrastructure.Identity.Api;
+
+public class Config {
+    public static IEnumerable<IdentityResource> IdentityResources =>
+        new List<IdentityResource> { };
+
+    public static IEnumerable<ApiScope> ApiScopes => new List<ApiScope> { };
+
+    public static IEnumerable<Client> Clients => new List<Client> { };
+}
--- Infrastructure/Identity.Api/Program.cs
+++ Infrastructure/Identity.Api/Program.cs
@@ -1,6 +1,16 @@
+using RecAll.Infrastructure.Identity.Api;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var services = builder.Services;
+
+services.AddIdentityServer()
+    .AddInMemoryIdentityResources(Config.IdentityResources)
+    .AddInMemoryApiScopes(Config.ApiScopes).AddInMemoryClients(Config.Clients)
+    .AddDeveloperSigningCredential();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.UseIdentityServer();
 
 app.Run();
