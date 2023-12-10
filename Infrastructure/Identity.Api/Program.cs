46ef35ce91cae3c20b09f51985129b407f5c3a1c
Zhang, Yin <zhangyin@mail.neu.edu.cn>
2023-12-10 16:08:25 +0800

    NA

--- Infrastructure/Identity.Api/Program.cs
+++ Infrastructure/Identity.Api/Program.cs
@@ -1,20 +1,36 @@
+using System.Reflection;
+using Identity.Api;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var services = builder.Services;
+var configuration = builder.Configuration;
+configuration.SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", false, true).AddEnvironmentVariables();
+
+var connectionString =
+    configuration.GetConnectionString("ApplicationDbContext");
+var migrationAssemblyString =
+    typeof(Program).GetTypeInfo().Assembly.GetName().Name;
 
 services.AddControllersWithViews();
 
-// services.AddIdentityServer()
-//     .AddInMemoryIdentityResources(Config.IdentityResources)
-//     .AddInMemoryApiScopes(Config.ApiScopes).AddInMemoryClients(Config.Clients)
-//     .AddDeveloperSigningCredential();
+services.AddIdentityServer().AddTestUsers(TestUsers.Users)
+    .AddConfigurationStore(o => o.ConfigureDbContext = ob =>
+        ob.UseSqlServer(connectionString,
+            sql => sql.MigrationsAssembly(migrationAssemblyString)))
+    .AddOperationalStore(o => o.ConfigureDbContext = ob =>
+        ob.UseSqlServer(connectionString,
+            sql => sql.MigrationsAssembly(migrationAssemblyString)))
+    .AddDeveloperSigningCredential();
 
 var app = builder.Build();
 
 app.UseIdentityServer();
 
 app.UseCookiePolicy(
-    new CookiePolicyOptions {MinimumSameSitePolicy = SameSiteMode.Lax});
+    new CookiePolicyOptions { MinimumSameSitePolicy = SameSiteMode.Lax });
 app.UseStaticFiles();
 app.UseRouting();
 app.UseAuthorization();
